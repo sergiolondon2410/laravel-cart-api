f9e5ac5ad73ce2d2c55700cef4331ad6d963012a
sergiolondon2410 <sergiolondon2410@gmail.com>
2023-01-16 22:41:11 -0300

    b01: External product API setup

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::controller(ProductController::class)->group(function (){
+//    Route::get('/products', 'index');
+//});
+
+Route::get('/products', [ProductController::class, 'index'])->name('products');
